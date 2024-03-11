1334cc88c5212eec2a59a8416a9eac3b0c335ccf
Christian O. Venegas <cvenegas@esri.com>
2024-03-11 14:59:15 -0700

    Add the RTC files for building and packaging

---
+++ conanfile_rtc.py
@@ -0,0 +1,24 @@
+from conans import ConanFile
+
+
+class OpenJPEG(ConanFile):
+    name = "openjpeg"
+    version = "2.5.2"
+    url = "https://github.com/Esri/openjpeg/tree/runtimecore"
+    license = "https://github.com/Esri/openjpeg/blob/runtimecore/LICENSE"
+    description = "OpenJPEG is an open-source JPEG 2000 codec written in C language."
+
+    # RTC specific triple
+    settings = "platform_architecture_target"
+
+    def package(self):
+        base = self.source_folder + "/"
+        relative = "3rdparty/openjpeg/"
+
+        # headers
+        self.copy("openjpeg.h", src=base + "src/lib/openjp2", dst=relative + "src/lib/openjp2")
+        self.copy("opj_config.h", src=base + "src/lib/openjp2", dst=relative + "src/lib/openjp2")
+
+        # libraries
+        output = "output/" + str(self.settings.platform_architecture_target) + "/staticlib"
+        self.copy("*" + self.name + "*", src=base + "../../" + output, dst=output)
